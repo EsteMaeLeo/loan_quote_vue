1e10804ebf3c5cc7c6c95579f69beeb32cb1ba52
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-16 22:57:47 -0600

    initial dev on project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-quto bg-white shadow p-10">
+    <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+      How much <span className="text-indigo-700">Money</span> do you need?
+    </h1>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
