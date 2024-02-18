767dddb62ff84fc67c34208719f5434aee43c0d8
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-17 21:19:46 -0600

    dev on project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-
+  import Header from './components/Header.vue';
 </script>
 
-<template>
+<template> 
   <div class="my-20 max-w-lg mx-quto bg-white shadow p-10">
-    <h1 className="text-4xl font-extrabold text-gray-500 text-center">
-      How much <span className="text-indigo-700">Money</span> do you need?
-    </h1>
+    <Header />
   </div>
   
 </template>
